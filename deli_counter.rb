72aa2d963de8c3a50809780b28e048c2d3e52a81
Grace Choi <oiograceoio@gmail.com>
2018-09-23 05:36:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,14 +3,14 @@ katz_deli = []
 
 def line(katz_deli)
   customers = 1
+  customer_in_line = []
   if katz_deli.size == 0
     puts "The line is currently empty."
   else
-    puts "The line is currently: "
     katz_deli.each do |customer|
-      print "#{customers}. "
-      print "#{customer} "
+      customer_in_line << "#{customers}. #{customer}"
       customers += 1
     end
+    puts "The line is currently: #{customer_in_line}"
   end
 end
