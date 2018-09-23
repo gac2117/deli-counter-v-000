88fc94164d8d514460fe2c41e18ea5aa674cd7d7
Grace Choi <oiograceoio@gmail.com>
2018-09-23 05:29:55 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
   else
-    print "The line is currently: "
+    puts "The line is currently: "
     katz_deli.each do |customer|
       print "#{customers}. "
       print "#{customer} "
