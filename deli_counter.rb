d3f64fac3fda04fab2919c5c96835ad87df79690
Grace Choi <oiograceoio@gmail.com>
2018-09-23 05:37:23 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     katz_deli.each do |customer|
-      customer_in_line << "#{customers}. #{customer}"
+      customer_in_line << "#{customers}. #{customer} "
       customers += 1
     end
     puts "The line is currently: #{customer_in_line.join}"
