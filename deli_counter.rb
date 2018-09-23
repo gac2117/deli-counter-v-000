ea271669ff4b3b42eb16925f925804230b84d9ee
Grace Choi <oiograceoio@gmail.com>
2018-09-23 05:21:19 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,10 +5,6 @@ def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
   else
-    customers_in_line = katz_deli.size
-    customers = 0
-    while customers < customers_in_line
-      puts "The line is currently: 1. #{katz_deli.join(" #{katz_deli.index}")}"
-    end
+    puts "The line is currently: 1. #{katz_deli.join(" #{katz_deli.index}")}"
   end
 end
